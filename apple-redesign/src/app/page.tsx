25809588377d01879fe96f15aced2a844668a1e5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-10 14:40:20 +0200

    Add watch section

--- apple-redesign/src/app/page.tsx
+++ apple-redesign/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero/Hero";
 import SectionIphone from "./components/hero/SectionIphone";
+import SectionWatch from "./components/hero/SectionWatch";
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <SectionIphone />
+            <SectionWatch />
         </div>
     );
 }
