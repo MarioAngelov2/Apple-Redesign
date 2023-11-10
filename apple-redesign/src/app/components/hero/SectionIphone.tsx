ad75e61bca4b597f68552e80622b0e627119be03
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-10 14:19:06 +0200

    Create iphone section component

---
+++ apple-redesign/src/app/components/hero/SectionIphone.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Container from "../Container";
+import Image from "next/image";
+import Link from "next/link";
+
+const SectionIphone = () => {
+    return (
+        <div className="bg-[#F5F4F7]">
+            <Container>
+                <div className="flex flex-col items-center justify-center text-black relative">
+                    <div className="z-10 absolute top-20 flex flex-col items-center gap-2 px-1">
+                        <h1 className="text-4xl md:text-6xl font-semibold">
+                            iPhone 15 
+                        </h1>
+                        <h2 className="text-xl md:text-2xl text-center">
+                            Нова камера. Нов дизайн. Чуднология.
+                        </h2>
+                        <Link
+                            className="text-lg md:text-xl text-blue-500 hover:underline underline-offset-1"
+                            href="/"
+                        >
+                            Научи повече &rarr;
+                        </Link>
+                    </div>
+                    <div className="flex h-[600px]">
+                    <Image
+                            src="/iphone15.jpeg"
+                            alt="iphoneHero"
+                            fill
+                            className="object-cover "
+                        />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default SectionIphone;
