719a0e6f0156273db831809a741ee892bec4afe5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-12 10:53:04 +0200

    Create links helper file

---
+++ apple-redesign/src/app/utils/data.ts
@@ -0,0 +1,11 @@
+export const navLinks = [
+    { title: "Mac", link: "/" },
+    { title: "iPad", link: "/" },
+    { title: "iPhone", link: "/" },
+    { title: "Watch", link: "/" },
+    { title: "AirPods", link: "/" },
+    { title: "TV", link: "/" },
+    { title: "Забавления", link: "/" },
+    { title: "Поддръжка", link: "/" },
+    { title: "Къде да купиш", link: "/" },
+];
