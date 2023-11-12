559808df907cafab30bd1ccf3222cc2ce4a92e42
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-12 15:31:11 +0200

    Update SectionWatch.tsx

--- apple-redesign/src/app/components/hero/SectionWatch.tsx
+++ apple-redesign/src/app/components/hero/SectionWatch.tsx
@@ -6,12 +6,12 @@ import { AiFillApple } from "react-icons/ai";
 
 const SectionWatch = () => {
     return (
-        <div className="bg-black">
+        <div data-testid="section-watch" className="bg-black">
             <Container>
                 <div className="flex flex-col items-center justify-center text-white relative">
                     <div className="z-10 absolute top-20 flex flex-col items-center gap-2 px-1">
                         <h1 className="text-4xl md:text-6xl font-semibold flex items-center">
-                            <AiFillApple />
+                            <AiFillApple data-testid="apple-icon"/>
                             WATCH
                         </h1>
                         <p className="text-red-500 text-lg font-light">SERIES 9</p>
@@ -28,7 +28,7 @@ const SectionWatch = () => {
                     <div className="flex h-[700px]">
                         <Image
                             src="/watchSeries.jpeg"
-                            alt="iphoneHero"
+                            alt="watchSeries"
                             width={1800}
                             height={700}
                             className="object-cover "
