2d227cf9525eb443ddd25043a3744eaa79751016
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-13 12:16:14 +0200

    Update data.ts

--- apple-redesign/src/app/utils/data.ts
+++ apple-redesign/src/app/utils/data.ts
@@ -9,3 +9,31 @@ export const navLinks = [
     { title: "Поддръжка", link: "/" },
     { title: "Къде да купиш", link: "/" },
 ];
+
+export const footerLinks1 = [
+    { title: "Mac", link: "/" },
+    { title: "iPad", link: "/" },
+    { title: "iPhone", link: "/" },
+    { title: "Watch", link: "/" },
+    { title: "TV", link: "/" },
+    { title: "Music", link: "/" },
+    { title: "AirPods", link: "/" },
+]
+
+export const footerLinks2 = [
+    { title: "Apple TV+", link: "/" },
+    { title: "Apple Music", link: "/" },
+    { title: "Apple Arcade", link: "/" },
+]
+
+export const footerLinks3 = [
+    { title: "Newsroom", link: "/" },
+    { title: "Apple Leadership", link: "/" },
+    { title: "Инвеститори", link: "/" },
+    { title: "Етичност и законообразност", link: "/" },
+]
+
+export const footerLinks4 = [
+    { title: "Управление на Apple ID", link: "/" },
+    { title: "iCloud.com", link: "/" },
+]
