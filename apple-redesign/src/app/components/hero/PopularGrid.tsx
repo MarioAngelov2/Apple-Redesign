d7329df7a84429a4dfa1901878be9dd64b16c1d0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-11-13 10:11:28 +0200

    Minor refactor

--- apple-redesign/src/app/components/hero/PopularGrid.tsx
+++ apple-redesign/src/app/components/hero/PopularGrid.tsx
@@ -10,18 +10,18 @@ const PopularGrid = () => {
                 {/* LEFT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-[#FCFBFE]">
                     <div className="z-10 absolute top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold flex items-center">
+                        <h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold flex items-center">
                             <AiFillApple size={40} />
                             WATCH
                         </h1>
-                        <p className="text-red-500 text-xs md:text-base font-light">
+                        <p className="text-red-500 text-xs md:text-sm font-light">
                             ULTRA 2
                         </p>
-                        <h2 className="text-md md:text-xl font-light">
+                        <h2 className="text-md md:text-lg font-light">
                             Приключения на друго ниво.
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1 font-light"
+                            className="text-sm md:text-lg text-blue-500 hover:underline underline-offset-1 font-light"
                             href="/"
                         >
                             Научи повече
@@ -40,18 +40,18 @@ const PopularGrid = () => {
                 {/* RIGHT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-[#FCFBFE]">
                     <div className="z-10 absolute top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold flex items-center">
+                        <h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold flex items-center">
                             <AiFillApple size={40} />
                             WATCH
                         </h1>
-                        <p className="text-red-500 text-xs md:text-base font-light">
+                        <p className="text-red-500 text-xs md:text-sm font-light">
                             SE
                         </p>
-                        <h2 className="text-md md:text-xl font-light">
+                        <h2 className="text-md md:text-lg font-light">
                             Много причини да го обикнеш.
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1 font-light"
+                            className="text-sm md:text-lg text-blue-500 hover:underline underline-offset-1 font-light"
                             href="/"
                         >
                             Научи повече
@@ -70,16 +70,16 @@ const PopularGrid = () => {
                 {/* BLACK SECTION */}
                 {/* LEFT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-black text-white">
-                    <div className="z-10 absolute top-10 md:top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold">
+                    <div className="z-10 absolute top-10 md:top-16 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
+                        <h1 className="text-2xl md:text-3xl xl:text-4xl font-semibold">
                             AirPods Pro
                         </h1>
-                        <h2 className="text-md md:text-xl text-center font-light">
+                        <h2 className="text-md md:text-lg lg:text-xl text-center font-light">
                             Сега с адаптивно аудио. <br />
                             Звучи страхотно.
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1 font-light"
+                            className="text-sm md:text-lg lg:text-xl text-blue-500 hover:underline underline-offset-1 font-light"
                             href="/"
                         >
                             Научи повече
@@ -97,11 +97,11 @@ const PopularGrid = () => {
                 </div>
                 {/* RIGHT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-black text-white">
-                    <div className="z-10 absolute top-10 md:top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold">
+                    <div className="z-10 absolute top-10 md:top-16 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
+                        <h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold">
                             iPad Pro
                         </h1>
-                        <h2 className="text-md md:text-xl flex gap-1 font-light">
+                        <h2 className="text-md md:text-lg flex gap-1 font-light">
                             Със суперсилите на{" "}
                             <span>
                                 <Image
@@ -113,7 +113,7 @@ const PopularGrid = () => {
                             </span>
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1 font-light"
+                            className="text-sm md:text-lg text-blue-500 hover:underline underline-offset-1 font-light"
                             href="/"
                         >
                             Научи повече
@@ -132,16 +132,16 @@ const PopularGrid = () => {
                 {/* WHITE SECTION */}
                 {/* LEFT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-[#FCFBFE]">
-                    <div className="z-10 absolute top-12 md:top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold">
+                    <div className="z-10 absolute top-8 md:top-6 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
+                        <h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold">
                             MacBook Air 15"
                         </h1>
-                        <h2 className="text-md md:text-xl text-center font-light">
+                        <h2 className="text-md md:text-lg text-center font-light">
                             Впечатляващо голям. <br />
                             Невъзможно тънък.
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1"
+                            className="text-sm md:text-lg text-blue-500 hover:underline underline-offset-1"
                             href="/"
                         >
                             Научи повече
@@ -153,23 +153,23 @@ const PopularGrid = () => {
                             alt="watch_ultra"
                             width={2000}
                             height={600}
-                            className="object-cover min-w-[1000px] md:min-w-[1100px] lg:min-w-[1400px] mt-12"
+                            className="object-cover min-w-[1000px] md:min-w-[1100px] lg:min-w-[1400pxmd:mt-0"
                         />
                     </div>
                 </div>
                 {/* RIGHT SECTION */}
                 <div className="flex justify-center items-center overflow-hidden max-w-full max-h-full relative bg-[#FCFBFE]">
-                    <div className="z-10 absolute top-20 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
-                        <h1 className="text-3xl md:text-4xl font-semibold flex items-center">
+                    <div className="z-10 absolute top-6 right-0 left-0 flex flex-col items-center justify-center gap-2 px-1">
+                        <h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold flex items-center">
                             <AiFillApple size={40} />
                             tv
                         </h1>
-                        <h2 className="text-md md:text-xl text-center font-light">
+                        <h2 className="text-md md:text-lg text-center font-light">
                             Apple изживяването. <br />
                             Пиршество за всички сетива.
                         </h2>
                         <Link
-                            className="text-sm md:text-xl text-blue-500 hover:underline underline-offset-1"
+                            className="text-sm md:text-lg text-blue-500 hover:underline underline-offset-1"
                             href="/"
                         >
                             Научи повече
@@ -181,7 +181,7 @@ const PopularGrid = () => {
                             alt="watch_ultra"
                             width={2000}
                             height={600}
-                            className="object-cover min-w-[1000px] md:min-w-[1100px] lg:min-w-[1400px] mt-12"
+                            className="object-cover min-w-[1000px] md:min-w-[1100px] lg:min-w-[1400px md:mt-0"
                         />
                     </div>
                 </div>
